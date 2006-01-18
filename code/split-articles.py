115ed87f6ce010681dafef1a349c0e5cbf97d412
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-01-18 22:00:09 +0000

    this splits the huge 7z file on stdin and creates numbered 7z files in cwd
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@5 fe7ecb3d-170a-0410-af80-b42c727aa7d5

---
+++ code/split-articles.py
@@ -0,0 +1,60 @@
+import sys
+import re
+try:
+	import psyco
+except:
+	pass
+import os
+
+BUFSIZE = 40960
+STARTTAG = '<page>'
+ENDTAG = '</page>'
+MINSIZE = 100 * 1024 * 1024
+SEVENZIP = "7z"
+
+# Method:
+## Read in the input XML in 4096-byte chunks, incrementing *byte-count*
+## always append the result to *growing-string*
+## See if it contains </page>
+## If it does, see if *byte-count* > 100M
+## if so, then:
+### increment *outfile-number*
+### append the string up to and including ENDTAG to *growing-string*
+### write *growing-string* to a file called *outfile-number*
+### set *growing-string* to everything after ENDTAG
+
+chunk = ''
+growing_string = ''
+outfile_number = 0
+
+def sevenzip(s, filename):
+	cmd = SEVENZIP + " -si a " + filename
+	pipe = os.popen(cmd, 'w')
+	pipe.write(s)
+	pipe.close()
+
+# filter out everything before the first STARTAG
+# this assumes BUFSIZE is big enough to include the first STARTTAG
+chunk = sys.stdin.read(BUFSIZE)
+trash, chunk = chunk.split(STARTTAG, 1) # a left split
+chunk = STARTTAG + chunk # STARTTAG removed by the split
+growing_string += chunk
+
+while chunk:
+	if ENDTAG in chunk:
+		if len(growing_string) > MINSIZE:
+			outfile_number += 1
+			# declare our intentions
+			print 'Yay!  Writing file number', outfile_number
+			# now we write
+			start, end = chunk.rsplit(ENDTAG, 1) # a right split
+			growing_string += start + ENDTAG # removed by the splitting operation
+			sevenzip(growing_string, str(outfile_number)) # compress it and save it
+			# now we initialize state for next hundred megs
+			growing_string = end
+	else:
+		growing_string += chunk
+	# in all cases
+	chunk = sys.stdin.read(BUFSIZE)
+	# because chunk is set last in this loop, and the next thing that happens is the while(),
+	# we can be sure that every last byte gets processed.
