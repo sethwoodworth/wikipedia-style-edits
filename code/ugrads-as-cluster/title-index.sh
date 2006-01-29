18201ab1fb38563859004e6cbe9c9a349d3397d9
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-01-29 22:42:01 +0000

    title index, hopefully
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@59 fe7ecb3d-170a-0410-af80-b42c727aa7d5

---
+++ code/ugrads-as-cluster/title-index.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+PROJECT=title-indices
+JOBWAIT=20m
+
+CHATDIR="/tmp/dnet/$PROJECT/chat/"
+RESULTS="/tmp/dnet/$PROJECT/results/"
+MAXJOBS=36 # 18 times two
+PRE_SLEEP=12
+
+# For each computer, clear the CHATDIR and RESULTS directories
+for comp in $(cat ~/dnet/machines); do
+ssh $comp rm -rf $CHATDIR
+ssh $comp rm -rf $RESULTS
+done
+# For each computer, create them blank
+for comp in $(cat ~/dnet/machines); do
+ssh $comp mkdir -p $CHATDIR
+ssh $comp mkdir -p $RESULTS
+done
+
+
+# randomly assign jobs to machines
+for file in $(ssh paulproteus@fomalhaut.acm.jhu.edu '(cd /mnt/paul/space/ ; ls) ' | ~/bin/bogosort -n )
+do
+JOBCOUNT=$(jobs | wc -l)
+if [[ $JOBCOUNT -ge $MAXJOBS ]]
+then
+sleep $JOBWAIT
+fi
+# no matter what, sleep for some seconds
+sleep $PRE_SLEEP
+MY_UGRAD=$(~/bin/bogosort -n ~/dnet/machines | head -n1)
+ssh $MY_UGRAD "scp paulproteus@fomalhaut.acm.jhu.edu:/mnt/paul/space/$file /tmp/$file ; hostname ;   ~/bin/7za e -so /tmp/$file | ( cd  ~/dnet/collab/code/ ; python create_title_index.py ) | gzip > $RESULTS/${file%.7z}.gz " 2>&1 | gzip > $CHATDIR/$file-chat  &
+echo $file started on $MY_UGRAD
+done
+
