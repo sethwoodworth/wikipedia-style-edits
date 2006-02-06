e8552962da56cdfc554691f112e8c1151e14727d
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-02-06 02:12:01 +0000

    Current version in use
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@76 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- code/split-articles.py
+++ code/split-articles.py
@@ -11,25 +11,55 @@ import os
 CHUNK_SIZE = 100 * 1024 * 1024
 STARTTAG = '<page>'
 ENDTAG = '</page>'
-SEVENZIP = "7z"
+SEVENZIP = "7za"
 
-def sevenzip(s, filename):
-	cmd = SEVENZIP + " -si a " + filename
-	pipe = os.popen(cmd, 'w')
-	pipe.write('<xml version="1.0" charset="utf-8">' + s + '</xml>') # without this, the result isn't valid XML, since it contains multiple top-level tags
-	pipe.close()
+class sevenpipe:
+	def __init__(self):
+		self.so_far = 0
+		self.filename = str(NEXT_OUTFILE)
+		if NEXT_OUTFILE > 577: # already did <= 577
+			cmd = SEVENZIP + " -si a " + self.filename
+		else: # do a no-op for the ones we don't care about
+			cmd = 'cat > /dev/null'
+		self.pipe = os.popen(cmd, 'w')
+		print "Created self:", self.filename
+		self.pipe.write('<xml version="1.0" charset="utf-8">\n')
+		self.buffer = []
+		self.flush()
+
+	def flush(self):
+		self.pipe.write(''.join(self.buffer))
+		self.buffered_now = 0
+		self.buffer = []
+
+	def write(self, s):
+		self.buffer.append(s)
+		self.buffered_now += len(s)
+		if self.buffered_now > 4096000: # 4000K buffer for no particular reason
+			self.flush()
+			print 'flushed at', self.so_far
+
+		self.so_far += len(s)
+
+	def close(self):
+		self.flush()
+		self.pipe.write("</xml>")
+		self.pipe.close()
+		global NEXT_OUTFILE
+		NEXT_OUTFILE += 1
 
 NEXT_OUTFILE = 1 # This is the first filename we'll use.  We'll increment it.
 LOOKS_USEFUL_YET = 0 # Set this to a true value when we've seen the first input
-THIS_CHUNK = '' # This stores the chunk that we want to save, or at least might want to save later
+THIS_CHUNK = sevenpipe() # This stores the chunk that we want to save, or at least might want to save later
 
 for line in sys.stdin:
 	# Once we've seen the STARTTAG, we know we're in business
 	if LOOKS_USEFUL_YET or (STARTTAG in line):
 		LOOKS_USEFUL_YET = 1
-		THIS_CHUNK += line
+		if not '</mediawiki>' in line:
+			THIS_CHUNK.write(line)
 		# Note that the iterator preserves the trailing '\n'.  That's so sweet of it.
-		if len(THIS_CHUNK) > CHUNK_SIZE:
+		if THIS_CHUNK.so_far > CHUNK_SIZE or '</mediawiki>' in line:
 			# It's huge!
 			# Q. Should we save it?
 			if ENDTAG in line:
@@ -41,23 +71,8 @@ for line in sys.stdin:
 				# declare our intentions
 				print 'Yay!  Writing file number', NEXT_OUTFILE
 				# now we write
-				sevenzip(THIS_CHUNK, str(NEXT_OUTFILE)) # compress it and save it
+				THIS_CHUNK.close() # compress it and save it
 				
-				NEXT_OUTFILE += 1 # Sorta necessary :-)
-				THIS_CHUNK = '' # Clear it!
-
-# Now, we've stopped reading.
-# The last <page> is probably still trapped inside THIS_CHUNK
-# So let's solve this fencepost problem:
-
-if ENDTAG in THIS_CHUNK:
-	# Do a right-split to ignore trailing junk after </page>
-	THIS_CHUNK = THIS_CHUNK.rsplit(ENDTAG, 1) + ENDTAG + '\n'
-
-	# declare our intentions
-	print 'Yay!  Writing final file, number', outfile_number
-	# now we write
-	sevenzip(THIS_CHUNK, str(outfile_number)) # compress it and save it
-	THIS_CHUNK = '' # Clear it!
+				THIS_CHUNK = sevenpipe() # Clear it!
 
 # Now I think the jig is up.
