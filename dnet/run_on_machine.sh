f189a3ed59e14940c5695c2824e2b169c76b0df9
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-06 04:16:32 +0000

    ...
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@141 fe7ecb3d-170a-0410-af80-b42c727aa7d5

---
+++ dnet/run_on_machine.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+number=$RANDOM
+let "number %= 40 "
+sleep $number
+FILE=$1
+number=$(echo $FILE | sed -r 's/([^0-9]*?)([0-9]+)(.*?)/\2/' )
+MACHINECOUNT=$(wc -l < ~/dnet/machines)
+let "number = $number % $MACHINECOUNT + 1 "
+MACHINE=$(cat ~/dnet/machines | head -n$number | tail -n1)
+shift
+scp $FILE $MACHINE:/tmp/
+ssh $MACHINE "$@"
+ssh $MACHINE rm /tmp/$FILE
