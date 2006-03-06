f457935e75c608b90b0a64911a84bc3ae9804280
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-06 16:10:14 +0000

    Explicit is better than implicit
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@152 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- dnet/run_on_machine.sh
+++ dnet/run_on_machine.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -xe
-number=$RANDOM
+number=${RANDOM}
 let "number = number % 20 + 1"
-sleep $number
+sleep ${number}
 FILE=$1
-#number=$(echo $FILE | sed -r 's/([^0-9]*?)([0-9]+)(.*?)/\2/' )
+#number=$(echo ${FILE} | sed -r 's/([^0-9]*?)([0-9]+)(.*?)/\2/' )
 MACHINECOUNT=$(wc -l < ~/dnet/machines)
-let "number = $number % $MACHINECOUNT + 1 "
-MACHINE=$(cat ~/dnet/machines | head -n$number | tail -n1)
+let "number = $number % ${MACHINECOUNT} + 1 "
+MACHINE=$(cat ~/dnet/machines | head -n${number} | tail -n1)
 shift
-scp $FILE $MACHINE:/tmp/
-ssh $MACHINE "$@"
-ssh $MACHINE rm /tmp/$FILE
-echo -n "Finished $FILE at "; date
+scp ${FILE} ${MACHINE}:/tmp/
+ssh ${MACHINE} "$@"
+ssh ${MACHINE} rm /tmp/${FILE}
+echo -n "Finished ${FILE} at "; date
