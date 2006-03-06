10007354975e64139616962d8f5f51194833369b
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-06 09:41:25 +0000

    trivial changes to run_on_machine.sh
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@146 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- dnet/run_on_machine.sh
+++ dnet/run_on_machine.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 number=$RANDOM
-let "number %= 40 "
+let "number %= 20 "
 sleep $number
 FILE=$1
-number=$(echo $FILE | sed -r 's/([^0-9]*?)([0-9]+)(.*?)/\2/' )
+#number=$(echo $FILE | sed -r 's/([^0-9]*?)([0-9]+)(.*?)/\2/' )
 MACHINECOUNT=$(wc -l < ~/dnet/machines)
 let "number = $number % $MACHINECOUNT + 1 "
 MACHINE=$(cat ~/dnet/machines | head -n$number | tail -n1)
