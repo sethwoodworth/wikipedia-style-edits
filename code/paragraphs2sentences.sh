17860d5ed08d06ade4a45c0efb2d42950abac951
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-21 09:08:20 +0000

    java location  plus heap size
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@163 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- code/paragraphs2sentences.sh
+++ code/paragraphs2sentences.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd java-based-opennlp-wrapper
 export CLASSPATH=.:needed-jars/maxent-2.4.0.jar:needed-jars/opennlp-tools-1.3.0.jar:needed-jars/resolver.jar:needed-jars/xercesImpl.jar:needed-jars/xercesSamples.jar:needed-jars/xml-apis.jar:needed-jars/xml-writer.jar:needed-jars/trove.jar
-exec java -Xmx1g -Dorg.xml.sax.driver=org.apache.xerces.parsers.SAXParser DataProcessor EnglishSD.bin.gz
+exec /usr/java/jdk/bin/java -Xmx1g -Dorg.xml.sax.driver=org.apache.xerces.parsers.SAXParser DataProcessor EnglishSD.bin.gz
