9aa8bf1520166027774f0ecc574ccc5943db421a
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-27 04:40:19 +0000

    1-from-2 matching ready.  Maybe even correct.
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@186 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- code/lib/sentence_matching.py
+++ code/lib/sentence_matching.py
@@ -124,8 +124,15 @@ def hunks2sentencepairs(hunks):
                 olds = [ not_this_hunk.olds for not_this_hunk in hunks if not_this_hunk is not hunk ] 
                 competitors = make_sorted_competitors(new = new, oldss = olds, oldslicelen=1)
                 KEEP_GOING = not append_good_competitor(src=competitors, dst=almost_ret, new_set=[new])
-#            if KEEP_GOING: # Then try 1-from-2 matching within this hunk
-#                competitors = 
+            if KEEP_GOING: # Then try 1-from-2 matching within this hunk
+                competitors = make_sorted_competitors(new = new, oldss = [hunk.olds], oldslicelen=2)
+                KEEP_GOING = not append_good_competitors(src=competitors, dst=almost_ret,new_set=[new])
+            if KEEP_GOING: # Then try 1-from-2 matching within this hunk
+                olds = [ not_this_hunk.olds for not_this_hunk in hunks if not_this_hunk is not hunk ]
+                competitors = make_sorted_competitors(new = new, oldss = olds, oldslicelen=2)
+                KEEP_GOING = not append_good_competitor(src=competitor, dst=almost_ret, new_set=[new])
+    # Whew.  Now, we just need 2-from-1 matching and we're set.
+    
     ret = [ HunkOfSentences(olds=old, news=new)
             for (old, new) in almost_ret ]
     return ret
