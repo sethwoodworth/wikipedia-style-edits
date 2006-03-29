4bbfd16191c4e0425ac8978639aa1c2c6047198d
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-29 19:40:31 +0000

    tokenize
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@232 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- code/lib/tokenize.py
+++ code/lib/tokenize.py
@@ -1,7 +1,10 @@
 # From www.sims.berkeley.edu:8000/courses/.../lecture3.ppt
+import commands
+import tempfile
+import os
 import string
-import nltk.token
-import nltk.tokenizer
+#import nltk.token
+#import nltk.tokenizer
 url = r'((http:\/\/)?[A-Za-z]+(\.[A-Za-z]+){1,3}(\/)?(:\d+)?)'
 hyphen = r'(\w+\-\s?\w+)'
 apostro = r'(\w+\'\w+)'
@@ -9,9 +12,9 @@ numbers = r'((\$|#)?\d+(\.)?\d+%?)'
 punct = r'([^\w\s]+)'
 wordr = r'(\w+)'
 regexp = '|'.join([url, hyphen, apostro, numbers, wordr, punct])
-RT = nltk.tokenizer.RegexpTokenizer(regexp, SUBTOKENS='WORDS')
+#RT = nltk.tokenizer.RegexpTokenizer(regexp, SUBTOKENS='WORDS')
 
-def tokenize(u):
+def nltk_tokenize(u):
     if type(u) == type(''):
         u = unicode(u)
     assert(type(u) == type(u''))
@@ -24,3 +27,23 @@ def tokenize(u):
         ret.extend(thing.values())
     return ret
 
+def treebank_tokenize_lines(u):
+    if type(u) == type(''):
+        u = unicode(u)
+    assert(type(u) == type(u''))
+    s = u.encode('utf-8') # still sad
+    # Holy secure temp file handling, Batman!
+    fd, fname = tempfile.mkstemp()
+    os.write(fd, s)
+    os.close(fd)
+    out = commands.getoutput("sed -f tokenizer.sed < " + fname)
+    unicode_out = unicode(out, 'utf-8')
+    os.unlink(fname)
+    return [line.strip().split(' ') for line in unicode_out.split('\n') if line]
+if __name__ == '__main__':
+    import sys
+    for line in sys.stdin:
+        val = treebank_tokenize_lines(line)
+        if val:
+            print ' '.join(treebank_tokenize_lines(line)[0])
+    
