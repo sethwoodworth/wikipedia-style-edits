553d7fbe38fa40a05930a8812822da3c81740c61
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-03-29 05:13:05 +0000

    Fixed, but I hate it.
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@219 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- code/lib/jurafsky_martin_perl_tokenizer.pl
+++ code/lib/jurafsky_martin_perl_tokenizer.pl
@@ -3,39 +3,46 @@
 ## Taken from http://www.cs.colorado.edu/~martin/SLP/Updates/3.pdf
 
 $letternumber = "[A-Za-z0-9]";
-$notletter = "[ˆA-Za-z0-9]";
-$alwayssep = "[\\?!()\";/\\|‘]";
-$clitic = "(’|:|-|’S|’D|’M|’LL|’RE|’VE|N’T|’s|’d|’m|’ll|’re|’ve|n’t)";
+$notletter = "[^A-Za-z0-9]";
+$alwayssep = "[\\?!()\";/\\[']";
+$clitic = "{'|:|-|'S|'D|'M|'LL|'RE|'VE|N'T|'s|'d|'m|'ll|'re|'ve|n't}";
+
 $abbr{"Co."} = 1; $abbr{"Dr."} = 1; $abbr{"Jan."} = 1; $abbr{"Feb."} = 1;
-while (<>){
-      # put whitespace around unambiguous separators
-      s/$alwayssep/ $& /g;
-      # put whitespace around commas that aren’t inside numbers
-      s/([ˆ0-9]),/$1 , /g;
-      s/,([ˆ0-9])/ , $1/g;
-      # distinguish singlequotes from apostrophes by
-      # segmenting off single quotes not preceded by letter
-      s/ˆ’/$& /g;
-      s/($notletter)’/$1 ’/g;
-      # segment off unambiguous word-final clitics and punctuation
-      s/$clitic$/ $&/g;
-      s/$clitic($notletter)/ $1 $2/g;
-    # now deal with periods. For each possible word
+
+while(<>) {
+
+    # put whitespace around unambiguous separators
+    s/$alwayssep/ $& /g;
+   
+    # put whitespace around commas that aren't inside numbers
+    s/([^0-9]),/$1 , /g;
+    s/,([^0-9])/ , $1/g;
+   
+    # distinguish singlequotes from apostrophes by
+    # segmenting off single quotes not preceded by letter
+    s/^'/$& /g;
+    s/($notletter)'/$1 '/g;
+    
+    #segment off unambiguous word-final clitics and punctuation
+    s/$clitic$/ $&/g;
+    s/$clitic($notletter)/ $1 $2/g;
+
+    # now deal with periods.  For each possible word
     @possiblewords=split(/\s+/,$_);
     foreach $word (@possiblewords) {
-        # if it ends in a period,
-        if (($word =˜ /$letternumber\./)
-               && !($abbr{$word}) # and isn’t on the abbreviation list
-                  # and isn’t a sequence of letters and periods (U.S.)
-                  # and doesn’t resemble an abbreviation (no vowels: Inc.)
-               && !($word =˜ /ˆ([A-Za-z]\.([A-Za-z]\.)+|[A-Z][bcdfghj-nptvxz]+\.)$/)) {
-            # then segment off the period
-            $word =˜ s/\.$/ \./;
-        }
-        # expand clitics
-        $word =˜s/’ve/have/;
-        $word =˜s/’m/am/;
-        print $word," ";
+	# if it ends in a period,
+	if (($word =~ /$letternumber\./)
+	    && !($abbr{$word}) # and isn't on the abbreviation list
+	    # and isn't a sequence of letters and periods (U.S.)
+	    # and doesn't resemble an abbreviation (no vowels: Inc.)
+	    && !($word =~ /^([A-Za-z]\.([A-Za-z]\.)+|[A-Z][bcdfghj-nptvxz]+\.)$/)) {
+	    # then segment off the period
+	    $word =~ s/\.$/ \./;
+	}
+	# expand clitics
+	$word =~s/'ve/have/;
+	$word =~s/'m/am/;
+	print $word," ";
     }
-  print "\n";
+    print "\n";
 }
