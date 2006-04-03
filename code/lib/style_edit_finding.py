f58e9543120e08dbc308f8d4da7e9aef12dbb1b2
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2006-04-03 12:20:28 +0000

    use smarter, faster tokenizer
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@279 fe7ecb3d-170a-0410-af80-b42c727aa7d5

--- code/lib/style_edit_finding.py
+++ code/lib/style_edit_finding.py
@@ -51,8 +51,8 @@ def is_hunk_style_edit(hunk):
     old_string = make_improved_old(old_string, new_string)
 
     # Then tokenize both
-    old_tokens = tokenize.treebank_tmpfile_sed(old_string)
-    new_tokens = tokenize.treebank_tmpfile_sed(new_string)
+    old_tokens = t.tokenize(old_string)
+    new_tokens = t.tokenize(new_string)
     
     new_index = 0
     old_index = 0
