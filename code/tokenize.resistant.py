b660ac52ee2c901bc5358e2af2fa8be2d43f6a4c
asheesh <asheesh@fe7ecb3d-170a-0410-af80-b42c727aa7d5>
2008-10-15 21:32:23 +0000

    Add a sed-based tokenizer
    git-svn-id: svn+ssh://nlp.cs.jhu.edu/home/remote/asheesh/collab@320 fe7ecb3d-170a-0410-af80-b42c727aa7d5

---
+++ code/tokenize.resistant.py
@@ -0,0 +1,61 @@
+# From www.sims.berkeley.edu:8000/courses/.../lecture3.ppt
+import commands
+import tempfile
+import sys
+import os
+import string
+import nltk.token
+import nltk.tokenizer
+url = r'((http:\/\/)?[A-Za-z]+(\.[A-Za-z]+){1,3}(\/)?(:\d+)?)'
+hyphen = r'(\w+\-\s?\w+)'
+apostro = r'(\w+\'\w+)'
+numbers = r'((\$|#)?\d+(\.)?\d+%?)'
+punct = r'([^\w\s]+)'
+wordr = r'(\w+)'
+regexp = '|'.join([url, hyphen, apostro, numbers, wordr, punct])
+RT = nltk.tokenizer.RegexpTokenizer(regexp, SUBTOKENS='WORDS')
+
+def nltk_tokenize(u):
+    if type(u) == type(''):
+        u = unicode(u)
+    assert(type(u) == type(u''))
+    s = u.encode('utf-8') # sad
+    t = nltk.token.Token(TEXT=s)
+    fromRT = RT.tokenize(t)
+    words = t['WORDS']
+    ret = []
+    for thing in words:
+        ret.extend(thing.values())
+    return ret
+
+import pexpect
+class TreebankSedExpecter:
+    def __init__(self):
+        self.sed = pexpect.spawn("sed", ["-f", "tokenizer.sed"])
+        self.sed.setecho(False)
+        self.sed.delaybeforesend = 0
+
+    def filter_line(self, u):
+        if type(u) == type(''):
+            u = unicode(u, 'utf-8')
+        assert(type(u) == type(u''))
+        #s = u.encode('utf-8') # still sad
+        self.sed.sendline(u.rstrip())
+        try:
+            out = self.sed.readline().rstrip()
+        except:
+            out = '' # OW MY HEAD
+            self.__init__()
+            print >> sys.stderr, "For some reason,", u.encode('utf-8'), "failed to get sedded."
+            out = nltk_tokenize(u.decode('utf-8'))
+        unicode_out = unicode(out, 'utf-8')
+        return unicode_out.strip()
+    def tokenize(self, u):
+        return self.filter_line(u).split()
+
+if __name__ == '__main__':
+    import sys
+    t = TreebankSedExpecter()
+    for line in sys.stdin:
+        print t.filter_line(line)
+    
